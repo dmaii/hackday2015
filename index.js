9108c3209f9d6db9dfdb03d486a74d619fd6b351
David Mai <david.mai@isocket.com>
2015-07-23 23:29:07 -0700

    finish orders api

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var router = express.Router();
+var propNames = require('./propNames')
 
 var app = express();
 
@@ -19,6 +20,20 @@ connection.connect();
 
 orderQuery = 'SELECT orders.external_id, line_items.uid, line_items.impressions, line_items.start_time, line_items.end_time, max(stats_item.impressions) from line_items, orders, line_item_stats, stats_item where channel_group_uid="G-LZUFZBYC7H" and line_item_stats.id = stats_item.line_item_stats_id and line_items.uid = line_item_stats.line_item_uid and line_items.order_uid = orders.uid and line_items.end_time > now() group by line_items.uid;';
 
+function days_between(date1, date2) {
+  // The number of milliseconds in one day
+  var ONE_DAY = 1000 * 60 * 60 * 24
+
+  // Convert both dates to milliseconds
+  var date1_ms = date1.getTime()
+  var date2_ms = date2.getTime()
+
+  // Calculate the difference in milliseconds
+  var difference_ms = Math.abs(date1_ms - date2_ms)
+
+  // Convert back to days and return
+  return Math.round(difference_ms/ONE_DAY)
+}
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -38,19 +53,32 @@ app.get('/orders', function(req, res) {
   connection.query(orderQuery, function(err, rows, fields) {
     finalRows = [];
     for (var i = 0; i < rows.length; i++) {
-      if (rows[i].external_id.indexOf('REMOVED_FROM_IFA_CAMPAIGN') === -1) {
+      var endDate = new Date(rows[i].end_time);
+
+      if (rows[i].external_id.indexOf('REMOVED_FROM_IFA_CAMPAIGN') === -1 && endDate > new Date()) {
         var campId = rows[i].external_id.replace('IFA_CAMPAIGN_', '');
         var row = {
           url: "http://advertisers.isocket.com/campaigns/" + campId,
           imprActual: rows[i]['max(stats_item.impressions)'],
           imprExpected: rows[i].impressions,
           startDate: rows[i].start_time,
-          endDate: rows[i].end_time
+          propertyName: propNames[rows[i].uid],
+          endDate: rows[i].end_time,
+          isHealthy: true
         };
-        finalRows.push(row);
+
+        var startEndDiff = days_between(new Date(row.startDate), endDate);
+        var nowEndDiff = days_between(new Date(), endDate);
+
+        var percent = nowEndDiff / startEndDiff;
+
+        if ((percent + .1) < (row.imprActual / row.imprExpected)) {
+          row['isHealthy'] = false;
+          finalRows.push(row);
+        }
       }
     }
-    res.json({rows: rows});
+    res.json({rows: finalRows});
   });
 });
 
