056ffd153e79ebad1fd2e542c324a5da2b712e70
David Mai <david.mai@isocket.com>
2015-07-23 17:20:54 -0700

    merge

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
 $(document).ready(function() {
 
+  $('#content').load('html/home.html');
+
   $('.js-menu').click(function() {
     $('.menu').toggleClass('hidden');
   });
 
+  $('.menu-home').click(function() {
+    $('#content').load('html/home.html');
+  });
 
-  //$('#content').load('html/chat.html');
+  $('.menu-chat').click(function() {
+    $('#content').load('html/chat.html');
+    $('.menu').toggleClass('hidden');
+  });
+
 });
