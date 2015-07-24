047fe7379e98baef43288596d98a4c7393c255db
Joel Kramer <jkramer@rubiconproject.com>
2015-07-23 20:38:33 -0700

    ...

--- js/main.js
+++ js/main.js
@@ -8,15 +8,22 @@ $(document).ready(function() {
 
   $('.menu-home').click(function() {
     $('#content').load('html/home.html');
+    $('.site-wrapper .cover-container .inner h2').html('RP Now');
   });
 
   $('.menu-chat').click(function() {
     $('#content').load('html/chat.html');
     $('.menu').toggleClass('hidden');
+    $('.site-wrapper .cover-container .inner h2').html('Chat');
   });
 
   $('.menu-inbox').click(function() {
     $('#content').load('html/inbox.html');
     $('.menu').toggleClass('hidden');
+    $('.site-wrapper .cover-container .inner h2').html('Inbox');
   });
+
+  function openBrowser(url) {
+    window.open(url);
+  }
 });
