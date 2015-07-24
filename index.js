7852b5fbaff5fe9931271948f06d24f6dd86eab9
David Mai <david.mai@isocket.com>
2015-07-23 19:12:00 -0700

    make node app

---
+++ index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var router = express.Router();
+
+var app = express();
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(__dirname));
+app.set('views', __dirname + '/html');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+router.get('/', function(req, res) {
+  res.render(__dirname + '/html/index.html');
+});
+
+/// catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+var server = app.listen(4000, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
+
+module.exports = app;
